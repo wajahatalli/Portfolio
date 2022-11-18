309bff75f3a59f6a93c0857c8518b21f19034bcd
wajahatalli <wajahat.jutt09@gmail.com>
2022-11-18 10:55:34 +0500

    Added som js for contact form validation

--- index.js
+++ index.js
@@ -118,3 +118,11 @@ window.addEventListener('DOMContentLoaded', () => {
     );
     document.getElementById('cards').innerHTML = displayCards.join('');
 });
+
+// Contact form validation script
+
+const form = document.querySelector('form');
+const emailAdrdress = document.getElementById('email');
+const emailPattern = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/g;
+const msgError = document.querySelector('#message-error');
+const msgSuccess = document.querySelector('#message-success');
