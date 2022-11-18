f538333c6001e737011321541aa6dc004885490b
AmanWorku <amexworku2014@gmail.com>
2022-11-18 08:41:59 +0300

    Add javascript for the popup window

--- index.js
+++ index.js
@@ -72,3 +72,49 @@ const onModalOpen = async (id) => {
     .map((tech) => `<li class="stack">${tech}</li>`)
     .join('');
 };
+
+const p = onModalOpen;
+
+const onModalClose = () => {
+  const modal = document.querySelector('.modal-container');
+  modal.style.display = 'none';
+};
+onModalClose(p);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const displayCards = projects.map(
+      (project) => `
+      <div id=${project.id} >
+        <div class="work-info1">
+            <div class="image-container">
+              <img
+                src=${project.image}
+                alt="multi-post work content2"
+                class="pic-temp1"
+              />
+            </div>
+            <div class="content1">
+              <h3 class="tonic">
+                ${project.name}
+              </h3>
+              <h4>
+                CANOPY <span class="dot tag"> Back End dev </span>
+                <span class="dot tag">2015</span>
+              </h4>
+              <p class="proj-info">
+              ${project.description}
+              </p>
+              <ul class="stack-icon">
+              ${project.technologies
+          .map((tech) => `<li class="stack">${tech}</li>`)
+          .join('')}
+              </ul>
+              <button id=${project.id} type="button" onclick="onModalOpen(${
+          project.id
+      })" class="btn" id="btn-4">See Project</button>
+            </div>
+          </div> 
+      </div>`,
+  );
+  document.getElementById('cards').innerHTML = displayCards.join('');
+});
